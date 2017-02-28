75a0180835c9500091d6eecb5d6945cb6e21ff51
elecho1 <rikiauczatu@gmail.com>
2017-02-28 14:46:08 +0900

    add 'about' page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   get 'index' => 'static_pages#index'
+  get 'about' => 'static_pages#about'
 
 
 
