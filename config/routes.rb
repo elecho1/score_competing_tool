b4cefc3d7a4c58fdaf6dee787d55b97a490834a8
elecho1 <rikiauczatu@gmail.com>
2017-03-02 12:41:07 +0900

    edited 'users/edit' page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   root 'static_pages#index'
   get 'index' => 'static_pages#index'
   get 'about' => 'static_pages#about'
