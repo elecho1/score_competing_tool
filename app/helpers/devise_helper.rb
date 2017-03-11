662c826a9d3e5c3ca24b2ea8c35ec01f9d582606
elecho1 <rikiauczatu@gmail.com>
2017-03-12 01:19:17 +0900

    customized devise error-messages for bootstrap

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,42 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    html = ""
+    html += <<-BOF1
+    <div class="alert alert-danger alert-dismissible" role="alert">
+    BOF1
+
+    sentence = I18n.t("errors.messages.not_saved", count: resource.errors.count, resource: I18n.t("attributes.#{resource.class.model_name.i18n_key}"))
+
+    html += <<-BOF2
+      #{sentence}
+      <ul>
+    BOF2
+    
+    messages = resource.errors.full_messages.each do |errmsg|
+      #html += <<-EOF
+      #<div class="alert alert-danger alert-dismissible" role="alert">
+      #  <button type="button" class="close" data-dismiss="alert">
+      #    <span aria-hidden="true">&times;</span>
+      #    <span class="sr-only">close</span>
+      #  </button>
+      #  #{errmsg}
+      #</div>
+      #EOF
+      html += <<-IOF
+          <li>#{errmsg}</li>
+      IOF
+    end
+    html += <<-EOF
+      </ul>
+    </div>
+    EOF
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    resource.errors.empty? ? false : true
+  end
+
+end
