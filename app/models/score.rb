2a5b9599800c817927757a0bb9fef85b79d76638
elecho1 <rikiauczatu@gmail.com>
2017-04-07 23:20:49 +0900

    score更新時のuser_scoreのtouchをやめた

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,6 +1,6 @@
 class Score < ActiveRecord::Base
   #association
-  belongs_to :user_score, touch: true#, counter_cache: :score_count
+  belongs_to :user_score#, touch: true#, counter_cache: :score_count
   belongs_to :subject
 
   #validation
