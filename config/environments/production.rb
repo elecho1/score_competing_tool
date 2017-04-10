6637c102d7f30e3e771f9ad33edfe191cebf31ee
elecho1 <rikiauczatu@gmail.com>
2017-04-10 21:35:02 +0900

    production環境のメール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { host: 'eeic-scores.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['CONFIRM_MAIL_ADDRESS'],
+    :password => ENV['CONFIRM_MAIL_PASSWORD'],
+    :authentication => 'login', #Action_Mailerのヘルプを参照
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
