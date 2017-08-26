c801bcacd36358a942efd7faf0250a60d204a635
elecho1 <rikiauczatu@gmail.com>
2017-08-26 18:13:46 +0900

    slackのOauthの一連の流れを実装（細かいユーザー情報の編集などはまだ）

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,19 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def slack
-    #binding.pry
     auth = request.env["omniauth.auth"]
-    binding.pry
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if auth.info.team_id.eql?(ENV['SLACK_TEAM_ID'])
+      @user = User.from_omniauth(auth)
+    else 
+      flash[:alert]="チームIDが間違っています"
+      failure()
+      return
+    end
 
-    #if @user.persisted?
-    #  sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    if @user.persisted?
+      flash[:notice] = "ログインに成功しました。"
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
     #  set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    #else
-    #  session["devise.facebook_data"] = request.env["omniauth.auth"]
-    #  redirect_to new_user_registration_url
-    #end
-    binding.pry
-    redirect_to root_path
+    else
+      session["devise.slack_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+    #binding.pry
+    #redirect_to root_path
   end
 
   def failure
