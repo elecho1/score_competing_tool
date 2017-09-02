78bb13489914278072b6cec74c8a053b68e1e1ac
elecho1 <rikiauczatu@gmail.com>
2017-09-02 18:22:59 +0900

    アカウント作成方式変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,23 +12,43 @@ class User < ActiveRecord::Base
   has_one :user_score, dependent: :destroy  
 
   #validation
-  validates :user_key, presence: true, uniqueness: true, 
-  format: {with: /\w*\z/}, 
-  length: {minimum: 2, maximum: 50}
+  #validates :user_key, presence: true, uniqueness: true, 
+  #format: {with: /\w*\z/}, 
+  #length: {minimum: 2, maximum: 50}
   validates :open_user_name, inclusion: {in: [true, false], message: "を選択して下さい"}
   validates :open_score, inclusion: {in: [true, false], message: "を選択して下さい"}
   validates :user_name, length: {maximum: 50, allow_blank: true}
 
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user = User.where(provider: auth.provider, uid: auth.uid).first
       #user.email = auth.info.email
-      #user.password = Devise.friendly_token[0,20]
+    unless user
+      user = User.create!(
+        email:self.dummy_email(auth),
+        password: Devise.friendly_token[0,20], user_key: self.dummy_user_key(auth),provider: auth.provider, uid: auth.uid, open_user_name: false, open_score: true
+      )
+    end 
       #user.name = auth.info.name   # assuming the user model has a name
       #user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
-      #user.skip_confirmation!
-    end
+      # user.skip_confirmation!
+    return user
   end
 
+  def self.dummy_user_key(auth)
+    "#{auth.uid}_#{auth.provider}"
+  end
+
+  def self.dummy_email(auth)
+    "#{auth.uid}_#{auth.provider}@example.com"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826075552) do
+ActiveRecord::Schema.define(version: 20170831114653) do
 
   create_table "scores", force: :cascade do |t|
     t.integer  "user_score_id", limit: 4, null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20170826075552) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: ""
-    t.string   "encrypted_password",     limit: 255, default: ""
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
