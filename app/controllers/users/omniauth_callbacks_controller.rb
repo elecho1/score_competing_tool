fbf4da9a7cc0ae2c503c17743bc2a0d3ffe726e5
elecho1 <rikiauczatu@gmail.com>
2017-09-03 18:35:23 +0900

    ログイン経路変更

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,23 +5,33 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.from_omniauth(auth)
     else 
       flash[:alert]="チームIDが間違っています"
-      failure()
+      redirect_to root_path
       return
     end
 
     if @user.persisted?
-      flash[:notice] = "ログインに成功しました。"
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in @user, :event => :authentication
+      if @user.slack_enabled_flag
+        flash[:notice] = "ログインに成功しました。"
+        redirect_to root_path
+      else 
+        flash[:notice] = "引き続きアカウント情報を登録してください。"
+        #redirect_to edit_user_registration_path
+      end
+      
+      #sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
     #  set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      session["devise.slack_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      #session["devise.slack_data"] = request.env["omniauth.auth"]
+      #redirect_to new_user_registration_url
+      failure()
     end
     #binding.pry
     #redirect_to root_path
   end
 
   def failure
+    flash[:alert] = "ログインに失敗しました。"
     redirect_to root_path
   end
 end
