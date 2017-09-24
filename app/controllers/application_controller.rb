852f0420028b48b13ba2ac1ccc14296fb152d711
elecho1 <rikiauczatu@gmail.com>
2017-09-24 13:58:39 +0900

    Basic認証廃止

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,9 +31,9 @@ class ApplicationController < ActionController::Base
   end
 
   def http_basic_authenticate
-    authenticate_or_request_with_http_basic  do |name, password|
-      name == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
+    #authenticate_or_request_with_http_basic  do |name, password|
+    #  name == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    #end
   end
 
 
