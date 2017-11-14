d078547fa9b9dbe6cb743523374ec55fafbea216
elecho1 <rikiauczatu@gmail.com>
2017-11-15 01:30:49 +0900

    modified validation

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,7 +1,4 @@
 class Lab < ActiveRecord::Base
   # association
   has_many :users
-
-  #validation
-  validates :lab_id, numericality: {only_integer: true}, inclusion: {in: Lab.select(:id)}, allow_nil: true
 end
