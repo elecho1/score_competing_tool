529ae8c88e563a84bd2f61f4f7307f359c95be99
elecho1 <rikiauczatu@gmail.com>
2017-11-15 23:42:26 +0900

    made contents of lab-page

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -4,6 +4,35 @@ class LabsController < ApplicationController
   before_action :registered_scores?
 
   def show
+    # self info
+    @mylab = current_user.lab
+    if @mylab
+      mylab_applicants = @mylab.users.includes(:user_score)
+      @mylab_reordered_applicants = mylab_applicants.sort{|a, b| b.user_score.total_score <=> a.user_score.total_score}
+      @my_standing = 1
+      @mylab_reordered_applicants.each do |applicant|
+        if applicant.user_score.total_score > current_user.user_score.total_score
+          @my_standing += 1
+        end
+      end
+    end
+
+    # everyone info
+    @users = User.all
+    @labs = Lab.all.order(:id)
+    @labs_info = Array.new(@labs.length)
+    for i in 0..(@labs.length-1) do
+      @labs_info[i] = Hash.new
+      @labs_info[i]['lab'] = @labs[i]
+      @labs_info[i]['num'] = 0
+    end
+    # @labs_applicants = Array.new(@labs.length, 0)
+    @users.each do |user|
+      if user.lab_id
+        this_lab_info = @labs_info.find{|lab_info| lab_info['lab'].id == user.lab_id}
+        this_lab_info['num'] += 1
+      end
+    end   
   end
 
   private
