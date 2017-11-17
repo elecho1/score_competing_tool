3a54529996f00ec966ec52d7e8665985b628767e
elecho1 <rikiauczatu@gmail.com>
2017-11-18 02:26:56 +0900

    add capacity of each lab

---
+++ db/subject_seeds_3A_other.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+### 3S other
+Subject.create(key: "アントレプレナーシップⅠ", weight: "1", semester: 5, subject_type: "other")
+Subject.create(key: "アントレプレナーシップⅡ", weight: "1", semester: 5, subject_type: "other")
+Subject.create(key: "先端技術と社会特別講義Ⅰ", weight: "2", semester: 5, subject_type: "other")
+Subject.create(key: "創造的プロジェクトⅠ", weight: "1.5", semester: 5, subject_type: "other")
+Subject.create(key: "創造的プロジェクトⅢ", weight: "1.5", semester: 5, subject_type: "other")
+Subject.create(key: "スパコンプログラミング", weight: "2", semester: 5, subject_type: "other")
+Subject.create(key: "メディコンⅠ", weight: "1", semester: 5, subject_type: "other")
+Subject.create(key: "VLSIアーキテクチャ", weight: "2", semester: 5, subject_type: "other")
+Subject.create(key: "VLSI設計工学", weight: "2", semester: 5, subject_type: "other")
