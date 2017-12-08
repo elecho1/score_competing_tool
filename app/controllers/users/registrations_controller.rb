4e7024a1435269f27e66a8a86dd50d8d60cabb04
elecho1 <rikiauczatu@gmail.com>
2017-12-09 00:31:12 +0900

    updated pages

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    resource.update_without_password(params)
+    resource.update_without_current_password(params)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -53,4 +53,19 @@ class User < ActiveRecord::Base
   def self.dummy_email(auth)
     "#{auth.uid}_#{auth.provider}@example.com"
   end
+
+  # http://easyramble.com/user-account-update-without-password-on-devise.html
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+ 
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+ 
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
 end
